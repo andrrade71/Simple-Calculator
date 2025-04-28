2d74e8880fdaf47ba7bd8a3dbf3ce23952985357
andrrade71 <vitordn.va@gmail.com>
2025-04-28 19:21:28 +0000

    Operand Buttons

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator')
 const clearButton = document.getElementById('clear')
 const equalsButton = document.getElementById('equals')
+
+let firstOperand = null;
+let operator = null;
+let waitingForSecondOperand = false;
  
 numberButtons.forEach(function(itemdaLista) {
-    itemdaLista.addEventListener('click', function(event) {
+    itemdaLista.addEventListener('click', function(event) { 
         const numeroClicado = event.target.textContent; 
-        displayElement.value += numeroClicado;
-    });
+        if (waitingForSecondOperand === true) { 
+            displayElement.value = numeroClicado;
+            waitingForSecondOperand = false; 
+        } else {
+            displayElement.value += numeroClicado; 
+        }
+    }); 
 });
 
 clearButton.addEventListener('click', function() {
 displayElement.value = '';
 });
+
+operatorButtons.forEach(function(itemdaLista) {
+    itemdaLista.addEventListener('click', function(event) {
+        const operadorClicado = event.target.textContent; 
+        firstOperand = displayElement.value;          
+        operator = operadorClicado;                   
+        waitingForSecondOperand = true;
+    });
+});
+
