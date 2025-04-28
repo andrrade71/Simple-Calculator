ed8b7170a208f1d925a013a87642812b876dd3a3
andrrade71 <vitordn.va@gmail.com>
2025-04-28 18:34:08 +0000

    number buttons function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var 
+const displayElement = document.getElementById('display')
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
+const clearButton = document.getElementById('clear')
+const equalsButton = document.getElementById('equals')
+ 
+numberButtons.forEach(function(itemdaLista) {
+    itemdaLista.addEventListener('click', function(event) {
+        const numeroClicado = event.target.textContent; 
+        displayElement.value += numeroClicado;
+    });
+});
